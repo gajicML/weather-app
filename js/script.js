3bb68aae373c376f031d65cfc0866ee18df3543f
Mladen <mla.gajic@gmail.com>
2019-09-05 17:39:19 +0200

    testing api getWeather function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 $( document ).ready(() => {
+
+	async function getWeather() {
+	  let response = await fetch(`https://api.met.no/weatherapi/locationforecast/1.9/?lat=44.7;lon=17.1`);
+	  let str = await response.text()
+	  return str;
+	}
+
+	getWeather()
+	    .then(str => $.parseXML(str))
+	    .then(data => {
+	        console.log(data);
+    })
+
 });
