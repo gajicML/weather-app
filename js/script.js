809fde39d92cb06e7aa2e6e15faf42b779f380d8
Mladen <mla.gajic@gmail.com>
2019-09-05 20:11:11 +0200

    checkRange function for input handling

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $( document ).ready(() => {
 
-	async function getWeather() {
-	  let response = await fetch(`https://api.met.no/weatherapi/locationforecast/1.9/?lat=44.7;lon=17.1`);
-	  let str = await response.text()
-	  return str;
-	}
+    async function getWeather(lon, lat) {
+        let response = await fetch(`https://api.met.no/weatherapi/locationforecast/1.9/?lat=${lat};lon=${lon}`);
+        let str = await response.text()
+        return str;
+    }
 
-	showData = (place) => {
+    $(".showData").click((e) => {
         let latitude = "";
         let longitude = "";
+        
+        let place = $(e.currentTarget).data('val');
 
         if(place == 0){
             latitude = $(".departure .lat").val();
@@ -18,14 +20,16 @@ $( document ).ready(() => {
             longitude = $(".destination .lon").val();
         }
 
+        if(!checkRange(latitude, longitude)) return false;
+        
         getWeather(longitude, latitude)
         .then(str => $.parseXML(str))
         .then(data => {
-            doTheWork(data, place)
+            displayData(data, place)
         })
-    };
+    });
 
-    doTheWork = (data, place) => {
+    displayData = (data, place) => {
         const workData = data.querySelectorAll("[datatype=forecast]")[0].childNodes[1];
 
         const props = workData.childNodes;
@@ -34,31 +38,31 @@ $( document ).ready(() => {
             if(props[i].nodeName != '#text'){
                 switch(props[i].nodeName){
                     case "temperature":
-                        document.getElementsByClassName("temperature")[place].innerHTML = props[i].getAttributeNode('value').value;
+                        document.getElementsByClassName("temperature")[place].textContent = props[i].getAttributeNode('value').value;
                         break;
 
                     case "dewpointTemperature":
-                        document.getElementsByClassName("dew")[place].innerHTML = props[i].getAttributeNode('value').value;
+                        document.getElementsByClassName("dew")[place].textContent = props[i].getAttributeNode('value').value;
                         break;
                     
                     case "humidity":
-                        document.getElementsByClassName("humidity")[place].innerHTML = props[i].getAttributeNode('value').value;
+                        document.getElementsByClassName("humidity")[place].textContent = props[i].getAttributeNode('value').value;
                         break;
 
                     case "fog":
-                        document.getElementsByClassName("fog")[place].innerHTML = props[i].getAttributeNode('percent').value;
+                        document.getElementsByClassName("fog")[place].textContent = props[i].getAttributeNode('percent').value;
                         break;
 
                     case "lowClouds":
-                        document.getElementsByClassName("low")[place].innerHTML = props[i].getAttributeNode('percent').value;
+                        document.getElementsByClassName("low")[place].textContent = props[i].getAttributeNode('percent').value;
                         break;
 
                     case "mediumClouds":
-                        document.getElementsByClassName("mid")[place].innerHTML = props[i].getAttributeNode('percent').value;
+                        document.getElementsByClassName("mid")[place].textContent = props[i].getAttributeNode('percent').value;
                         break;
 
                     case "highClouds":
-                        document.getElementsByClassName("high")[place].innerHTML = props[i].getAttributeNode('percent').value;
+                        document.getElementsByClassName("high")[place].textContent = props[i].getAttributeNode('percent').value;
                         break;
                 }
             }
@@ -68,4 +72,31 @@ $( document ).ready(() => {
 
     }
 
+    checkRange = (latitude, longitude) => {
+        if(latitude == ""){
+            alert('Latitude field cannot be empty')
+            return false;
+        } else if(longitude == ""){
+            alert('Longitude field cannot be empty')
+            return false;
+        }
+
+        latitude = parseFloat(latitude);
+        longitude = parseFloat(longitude);
+        
+        if(latitude >= 80 || latitude <= -80){
+            alert("Latitude range must be from -80 to 80")
+            return false;
+        }
+        if(longitude >= 180 || longitude <= -180){
+            alert("Longitude range must be from -180 to 180")
+            return false;
+        }
+        
+        return true;
+    }
+
+
 });
+
+
